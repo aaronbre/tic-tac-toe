affaf2d85f84ef94c24984ba88a713299de071a9
aaronbre <aribrecherdev@gmail.com>
2016-07-05 14:55:55 -0400

    added jquery bootstrap and normalize

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 /**
  * Created by aaronbrecher on 7/4/16.
  */
+
+var game = {
+  movesLeft: [
+      ["topLeft", "topCenter", "topRight"],
+      ["middleLeft", "middleCenter", "middleRigth"],
+      ["bottomRight", "bottomCenter", "bottomRight"]
+  ],
+};
+
+$(document).ready(function () {
+    
+});
